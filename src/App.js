ab1e9d32f1c457ba486182e8a30c43cb5e18d777
hongui <honge7694@naver.com>
2024-01-08 12:05:00 +0900

    login state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useEffect } from "react";
-
-// react-router components
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Route, Routes, Link, Navigate, useLocation } from 'react-router-dom';
 
 // @mui material components
 import { ThemeProvider } from "@mui/material/styles";
@@ -18,12 +16,22 @@ import ContactUsPage from 'layouts/pages/landing-pages/contact-us';
 
 export default function App() {
   const { pathname } = useLocation();
+  const [isLoggedIn, setLoggedIn] = useState(false);
 
   // Setting page scroll to 0 when changing the route
   useEffect(() => {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
+    // 로컬 스토리지에서 토큰을 가져와 로그인 여부를 확인
+    const token = localStorage.getItem('token');
+    setLoggedIn(!!token);
   }, [pathname]);
+  
+  const handleLogout = () => {
+    // 로그아웃 처리 로직
+    localStorage.removeItem('token');
+    setLoggedIn(false);
+  };
 
   const getRoutes = (allRoutes) =>
     allRoutes.map((route) => {
@@ -44,8 +52,18 @@ export default function App() {
       <Routes>
         {getRoutes(routes)}
         <Route path="/presentation" element={<Presentation />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<ContactUsPage />} />
+        {isLoggedIn ? (
+          <Route color="inherit" component={Link} to="/logout" onClick={handleLogout}>
+            Logout
+          </Route>
+        ) : (
+          <>
+            <Route path="/signin" element={<SignInPage />} />
+            <Route path="/signup" element={<ContactUsPage />} />
+          </>
+        )}
+        {/* <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<ContactUsPage />} /> */}
         {/* <Route path="*" element={<Navigate to="/presentation" />} /> */}
       </Routes>
     </ThemeProvider>
